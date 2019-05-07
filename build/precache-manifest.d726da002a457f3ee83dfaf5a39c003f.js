65ff43ad2cab64192610c55c401fc43f24110588
Ruben Bernardes <ruben@redcarrots.be>
2019-05-07 12:29:19 +0200

    test(): url in prod

---
+++ build/precache-manifest.d726da002a457f3ee83dfaf5a39c003f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dcc3101e45f750083e",
+    "url": "/static/css/main.fa87474d.chunk.css"
+  },
+  {
+    "revision": "65dcc3101e45f750083e",
+    "url": "/static/js/main.04fd222a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77f6a544b7733366c69e",
+    "url": "/static/js/2.3ec4e435.chunk.js"
+  },
+  {
+    "revision": "9856c8a567f2e79f8b5eb18378cb99da",
+    "url": "/static/media/logo.9856c8a5.png"
+  },
+  {
+    "revision": "26dadd093740590e4ecbd491c14ee457",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b54c0afca72916d8b250de18ca94794b.js"
+  "/precache-manifest.d726da002a457f3ee83dfaf5a39c003f.js"
 );
 
 workbox.clientsClaim();
